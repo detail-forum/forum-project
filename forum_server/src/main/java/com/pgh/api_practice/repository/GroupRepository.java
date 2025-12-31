3758bc6cbecb0145b697dba6c85dc791392cd748
rjsgud49 <qkrrjsgud49@gmail.com>
2025-12-31 17:32:58 +0900

    Update GroupRepository.java

--- forum_server/src/main/java/com/pgh/api_practice/repository/GroupRepository.java
+++ forum_server/src/main/java/com/pgh/api_practice/repository/GroupRepository.java
@@ -4,17 +4,10 @@ import com.pgh.api_practice.entity.Group;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
     Optional<Group> findByIdAndIsDeletedFalse(Long id);
     Page<Group> findByIsDeletedFalseOrderByCreatedTimeDesc(Pageable pageable);
-    
-    @Modifying
-    @Query("UPDATE Group g SET g.views = g.views + 1 WHERE g.id = :id")
-    void incrementViews(@Param("id") Long id);
 }
