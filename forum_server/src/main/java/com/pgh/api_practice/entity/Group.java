d4e8a5a6861a226dac942a13065f6255065ff67a
rjsgud49 <qkrrjsgud49@gmail.com>
2025-12-31 17:41:07 +0900

    Update Group.java

--- forum_server/src/main/java/com/pgh/api_practice/entity/Group.java
+++ forum_server/src/main/java/com/pgh/api_practice/entity/Group.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "groups")
+@Table(name = "user_groups")
 @Getter
 @Setter
 @NoArgsConstructor
