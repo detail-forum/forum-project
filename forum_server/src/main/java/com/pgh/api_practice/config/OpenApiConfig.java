9abdf720e6676df96044a532ee6b7a79b131d51d
송주영 <russeldestiny1234@gmail.com>
2026-01-08 15:16:02 +0900

    swagger 설정

--- forum_server/src/main/java/com/pgh/api_practice/config/OpenApiConfig.java
+++ forum_server/src/main/java/com/pgh/api_practice/config/OpenApiConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +18,18 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                // 🔐 Authorize 버튼 활성화
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(
+                        new io.swagger.v3.oas.models.Components()
+                                .addSecuritySchemes("BearerAuth",
+                                        new SecurityScheme()
+                                                .name("Authorization")
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
                 .info(new Info()
                         .title("Forum API")
                         .version("1.0.0")
